681509a0c0af3455edc60968132fb04f296d3801
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-12-24 17:35:32 -0600

    Add SPDX License Identifier to the license notice header

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2019-2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Point2D GA.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 worker.onmessage = e => {
     console.log(4)
     worker.pushMessage('ke')
