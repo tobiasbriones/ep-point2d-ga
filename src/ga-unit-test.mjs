683bdf5089f2a1e23f32962a2b5fd27fceb0c70f
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-12-27 13:39:02 -0600

    Add ga-unit-test.mjs skeleton

---
+++ src/ga-unit-test.mjs
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019-2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Point2D GA.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+export const gaTest = { run };
+
+function run() {
+  testPopulationCluster();
+  testFunctions();
+}
+
+function testPopulationCluster() {
+
+}
+
+function testFunctions() {
+
+}
