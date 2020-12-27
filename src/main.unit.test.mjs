b814ee801a4739a4417dbf4bc56ed5611753b20c
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-12-27 16:20:58 -0600

    Add main.unit.test.mjs

---
+++ src/main.unit.test.mjs
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Point2D GA.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import { populationTest } from './ga/population-unit-test.mjs';
+
+populationTest.run();
